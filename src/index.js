35ed8e3d85b26bbaaec19a415d89e6979976e897
Jindřiška Paďourková <padourkova.jindriska@gmail.com>
2021-04-24 11:45:49 +0200

    zprovoznění nnavigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './style.css';
 
 console.log('funguju!');
+
+const nav = document.querySelector('#nav-btn');
+
+nav.addEventListener('click', (e) => {
+  const navigation = document.querySelector('nav');
+  navigation.classList.toggle('nav-closed');
+});
