07500d72a671a639704ce957a7fc182a6879b988
Jindřiška Paďourková <padourkova.jindriska@gmail.com>
2021-04-24 12:44:59 +0200

    sbalování navigace

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import './style.css';
 console.log('funguju!');
 
 const nav = document.querySelector('#nav-btn');
+const navigation = document.querySelector('nav');
+const allItems = document.querySelectorAll('.nav_link');
 
 nav.addEventListener('click', (e) => {
-  const navigation = document.querySelector('nav');
   navigation.classList.toggle('nav-closed');
+});
+
+allItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    navigation.classList.toggle('nav-closed');
+  });
+});
+
+const order = document.querySelector('.order-btn');
+const selectedOrder = document.querySelector('.drink__cup');
+
+order.addEventListener('click', (e) => {
+  selectedOrder.classList.toggle('drink__cup--selected');
+  order.textContent = 'Zrušit';
 });
